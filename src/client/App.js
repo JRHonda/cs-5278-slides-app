c26addeb1ad45f18ca6a1f6f8a53728ee7d261aa
Justin Honda <justinohonda@gmail.com>
2019-10-02 19:13:25 -0400

    ghpages

--- src/client/App.js
+++ src/client/App.js
@@ -14,6 +14,8 @@ class App extends React.Component {
 
         link.href = URL.createObjectURL(blob);
 
+        console.log(link.href);
+
         this.setState(state => {
             const list = state.list.concat(link.href);
             return {
@@ -23,23 +25,23 @@ class App extends React.Component {
         });
     }
 
-    getImages = () => {
+    getImages = async () => {
 
         var i;
-        for(i = 1; i < 12; i++) {
+        for(i = 1; i < 98; i++) {
 
         var proxyUrl = 'https://cors-anywhere.herokuapp.com/',
-            targetUrl = `https://github.com/JRHonda/cs-5278-slides/blob/master/week-7-slide-${i}.png?raw=true`
+            //targetUrl ='https://github.com/JRHonda/cs-5278-slides/blob/master/cs5278-week-7-slides-pdf.pdf?raw=true';
+            targetUrl = `https://github.com/JRHonda/cs-5278-slides/blob/master/week-7-slide-${i}.png?raw=true`;
 
-        fetch(proxyUrl + targetUrl)
+        await fetch(proxyUrl + targetUrl)
             .then( response => response.blob() )
             .then( blob =>{
                 var reader = new FileReader() ;
                 var res = '';
-                reader.onload = function(){ res = this.result; } ; // <--- `this.result` contains a base64 data URI
+                reader.onload = function(){ console.log(this.result); res = this.result; } ; // <--- `this.result` contains a base64 data URI
                 console.log(res);
                 this.handleRes(blob);
-
                 reader.readAsDataURL(blob) ;
             }) ;
 
@@ -48,17 +50,20 @@ class App extends React.Component {
 
     }
 
+
     componentDidMount = () => {
         this.getImages();
     }
+
+
 
     render() {
         return (
 
             <div>
-                <h1 style={{fontSize: '32px'}}>Week 7</h1>
+                <h1 className={"ui segment blue"} style={{fontSize: '80px', display: 'flex', justifyContent: 'center', backgroundColor: 'lightgrey'}}>Week 7</h1>
                 {this.state.list.map(slide => (
-                    <img className={"ui container image"} src={slide} alt={""} />
+                    <img key={Math.random().toString(36).substr(2, 16)} className={"ui container image"} src={slide} alt={""} />
                 ))}
 
             </div>
